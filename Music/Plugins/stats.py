ae1d1e8ebbb3bb22fb05fb3ffd2f9140ea34c6b8
orekixadi <87181511+orekixadi@users.noreply.github.com>
2021-11-29 13:05:11 +0530

    Update stats.py

--- Music/Plugins/stats.py
+++ Music/Plugins/stats.py
@@ -1,4 +1,4 @@
-from Music import app, SUDOERS, BOT_ID
+from Music import app, SUDOUSERS, BOT_ID
 from pyrogram import filters, Client
 from sys import version as pyver
 from pyrogram import __version__ as pyrover
@@ -7,10 +7,10 @@ from pyrogram.types import Message
 import platform, socket, re, uuid, json, psutil, logging
 from Music.MusicUtilities.database.gbanned import get_gbans_count
 from Music.MusicUtilities.database.chats import get_served_chats
-from Music.MusicUtilities.database.sudo import (get_sudoers, get_sudoers, remove_sudo)
+from Music.MusicUtilities.database.sudo import (get_sudousers, get_sudousers, remove_sudo)
 from Music.MusicUtilities.database.playlist import get_playlist_count
 from Music.MusicUtilities.helpers.time import get_readable_time
-from Music import app, SUDOERS, Music_START_TIME
+from Music import app, SUDOUSERS, Music_START_TIME
 import os 
 import time
 from pymongo import MongoClient
@@ -46,7 +46,7 @@ async def gstats(_, message):
     free = (hdd.free / (1024.0 ** 3))
     free = str(free)
     msg = f"""
-**Global Stats of **__MentosMusic__** Bot**:\n\n
+**Global Stats of **__orekixMusic__** Bot**:\n\n
 [•]<u>__**System Stats**__</u>
 **Music Uptime:** {uptime}
 **System Process:** Online
