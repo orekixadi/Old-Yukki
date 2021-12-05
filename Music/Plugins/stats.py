3303a3951acc638419c25a59503d19ac840e94ea
iLOSTinYOU <87181511+orekixadi@users.noreply.github.com>
2021-12-05 14:23:35 +0530

    Update stats.py

--- Music/Plugins/stats.py
+++ Music/Plugins/stats.py
@@ -46,7 +46,7 @@ async def gstats(_, message):
     free = (hdd.free / (1024.0 ** 3))
     free = str(free)
     msg = f"""
-**Global Stats of **__MentosMusic__** Bot**:\n\n
+**Global Stats of **__OREKIxMUSIC__** Bot**:\n\n
 [•]<u>__**System Stats**__</u>
 **Music Uptime:** {uptime}
 **System Process:** Online
